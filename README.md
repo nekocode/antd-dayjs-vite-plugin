eaf814081fdd4dc320c2f2e6dfb5d853224fb7e6
nekocode <nekocode.cn@gmail.com>
2021-07-15 14:54:44 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![npm](https://img.shields.io/npm/v/antd-dayjs-vite-plugin)](https://www.npmjs.com/package/antd-dayjs-vite-plugin) ![npm bundle size](https://img.shields.io/bundlephobia/minzip/antd-dayjs-vite-plugin)
+
+A vite port of [antd-dayjs-webpack-plugin](https://github.com/ant-design/antd-dayjs-webpack-plugin). Day.js vite plugin for Ant Design (antd). 
+
+`yarn add antd-dayjs-vite-plugin`
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import type { Plugin } from 'vite';
+
+const ENTRY_FILE_NAME = 'init-dayjs-webpack-plugin-entry.js';
+
+export interface Options {
+  preset?: string;
+  plugins?: string[];
+  replaceMoment?: boolean;
+}
+
+export default function antdDayjs(
+  options: Options = { preset: 'antd' },
+): Plugin {
+  const { plugins, replaceMoment } = new (require('antd-dayjs-webpack-plugin'))(
+    options,
+  ) as { plugins: string[]; replaceMoment: boolean };
+
+  return {
+    name: 'test',
+
+    config(config) {
+      return replaceMoment === true
+        ? {
+            ...config,
+            resolve: {
+              ...config.resolve,
+              alias: {
+                ...config.resolve?.alias,
+                moment: 'dayjs',
+              },
+            },
+          }
+        : config;
+    },
+
+    transformIndexHtml(html) {
+      const segments = html.split('<body>');
+      return segments.join(
+        `<body><script type="module" src="/${ENTRY_FILE_NAME}"></script>`,
+      );
+    },
+
+    load(this, id) {
+      if (id.endsWith(ENTRY_FILE_NAME)) {
+        let code = `import * as dayjs from 'dayjs/dayjs.min';`;
+
+        plugins.forEach((plugin) => {
+          code += `import ${plugin} from 'dayjs/plugin/${plugin}';`;
+        });
+        code += `import antdPlugin from 'antd-dayjs-webpack-plugin/src/antd-plugin';`;
+
+        plugins.forEach((plugin) => {
+          code += `dayjs.extend(${plugin});`;
+        });
+        code += `dayjs.extend(antdPlugin);`;
+
+        return code;
+      }
+    },
+  };
+}
